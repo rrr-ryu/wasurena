2e0d1363cb3e812cc8745ceb95e633e08bfe7c30
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-19 22:36:48 +0900

    新規登録、ログイン画面の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_sign_up_parameters, if: :devise_controller?
+  before_action :configure_permitted_sign_in_parameters, if: :devise_controller?
 
 
   private
@@ -9,4 +11,13 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む
     end
   end
+
+  def configure_permitted_sign_up_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :uid])
+  end
+
+  def configure_permitted_sign_in_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :uid])
+  end
+
 end
