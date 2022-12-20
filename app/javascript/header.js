f408b05c936ce51ddabc9e263b8e75ca69a17893
rrr-ryu <zero.requiem910219@gmail.com>
2022-12-20 12:41:06 +0900

    ヘッダーjavascript作成

---
+++ app/javascript/header.js
@@ -0,0 +1,20 @@
+function headerList (){
+  const pullDownButton = document.getElementById('header-user-name')
+  const pullDownList = document.getElementById('hidden')
+
+  pullDownButton.addEventListener('mouseover', () => {
+    pullDownButton.setAttribute("style", "background-color: #EEEEEE");
+  });
+  pullDownButton.addEventListener('mouseleave', () => {
+    pullDownButton.removeAttribute("style");
+  });
+  pullDownButton.addEventListener('click', () => {
+    if (pullDownList.getAttribute("style") == "display: block;"){
+      pullDownList.removeAttribute("style")
+    } else {
+      pullDownList.setAttribute("style", "display: block;");
+    }
+  });
+};
+
+window.addEventListener('load', headerList);
